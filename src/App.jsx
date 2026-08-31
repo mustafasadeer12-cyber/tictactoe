645c5a1314232c69c87809f132ba7e2151bed3ae
mustafasadeer12-cyber <mustafasadeer12@gmail.com>
2026-08-30 18:18:08 -0700

    add: ol with nested li's, apply existing CSS classes

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,15 @@ function App() {
   
   return ( <main>
     <div id="game-container">
-      <ol>
-
+      <ol id="players">
+        <li>
+          <span className="player-name">Player 1 </span>
+          <span className="player-symbol">X</span>
+          </li>
+        <li>
+          <span className="player-name">Player 2 </span>
+          <span className="player-symbol">O</span>
+          </li>
       </ol>
 
       GAMEBOARD
