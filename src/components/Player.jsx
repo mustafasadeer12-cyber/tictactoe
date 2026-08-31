eb3d76607d5930852130aaad767bd76b5ef5030a
mustafasadeer12-cyber <mustafasadeer12@gmail.com>
2026-08-30 21:07:36 -0700

    experiment: wrap input+button in fragment to test ternary limits

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -5,10 +5,20 @@ export default function Player({ name, symbol }) {
 
     const handleEditClick = () => setIsEditing(!isEditing)
     
+
     return (
         <li>
           <span className="player">
-            { isEditing ? <input type="text" />: <span className="player-name"> { name } </span>}
+            { isEditing ?
+            (
+            <>
+            <input type="text" />
+            <button>Save</button>
+            </>
+            ) :
+             <span className="player-name"> { name } </span>}
+
+
               <span className="player-symbol">{symbol}</span>
               </span>
               <button onClick={handleEditClick}>Edit</button>
@@ -21,4 +31,10 @@ export default function Player({ name, symbol }) {
     //     <span className="player-name">{name}</span>
     // } else {
     //     <input>{ name }</input>
+    // }
+
+
+    // let playerName = <span className="player-name"> { name } </span>
+    // if(isEditing) {
+    //     playerName = <input type="text" className="player-name" />
     // }
