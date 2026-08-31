e7aee8e4f493263a600c90d7a7a0d675c67683cb
mustafasadeer12-cyber <mustafasadeer12@gmail.com>
2026-08-30 21:27:04 -0700

    refactor: switch ternary to if/let for scalability, cleaner multi-element returns

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -5,21 +5,21 @@ export default function Player({ name, symbol }) {
 
     const handleEditClick = () => setIsEditing(!isEditing)
     
+    let playerName = <span className="player-name"> { name } </span>
+    if(isEditing) {
+        playerName = (
+            <>
+            <input type="text"  required/>
+            <button onClick={handleSave}>Save</button>
+            </>
+        )
+    }
 
     return (
         <li>
           <span className="player">
-            { isEditing ?
-            (
-            <>
-            <input type="text" />
-            <button>Save</button>
-            </>
-            ) :
-             <span className="player-name"> { name } </span>}
-
-
-              <span className="player-symbol">{symbol}</span>
+             {playerName}
+             <span className="player-symbol">{ symbol }</span>
               </span>
               <button onClick={handleEditClick}>Edit</button>
           </li>
