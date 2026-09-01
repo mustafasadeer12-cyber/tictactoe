a8140e712accc0829d37893500c9b0518c51e786
mustafasadeer12-cyber <mustafasadeer12@gmail.com>
2026-09-01 08:26:19 -0700

    feat: add keyHandler function

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -2,19 +2,32 @@ import { useState } from 'react'
 
 export default function Player({ name, symbol }) {
     const [isEditing, setIsEditing] = useState(false)
+    const [player, setPlayer] = useState("")
 
+    // function saveName(e) {
+    //     const value = e.target.value // ANOTHER APPROACH.
+    //     setPlayer(value)
+    // }
+
+    function handleKey(e) {
+        if(e.key === 'Enter') {
+            setIsEditing(false)
+        }
+    }
+
+    const handleChange = (e) => {
+        setPlayer(e.target.value)
+    }
     function handleEditClick() {
-        setIsEditing(isEditing ? false : true)
+        setIsEditing((editing) => !editing) 
+        
     }
     
-
-
-    let playerName = <span className="player-name"> { name } </span>
+    let playerName = <span className="player-name"> { player } </span>
     if(isEditing) {
         playerName = (
             <>
-            <input type="text" required value={name} />
-            
+            <input type="text" required onChange={handleChange} value={player} onKeyDown={handleKey} />
             </>
         )
     }
