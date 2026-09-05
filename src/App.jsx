401644ac2672712b0c53cf25d75dbe49e9571b44
mustafasadeer12-cyber <mustafasadeer12@gmail.com>
2026-09-04 21:10:52 -0700

    debug #1

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,26 @@ import Player from './components/Player'
 import GameBoard from './components/GameBoard'
 import Log from './components/Log'
 
-function App() {
-  const [gameTurns, setGameTurns] = useState([])
-  const [activePlayer, setIsActivePlayer] = useState('X')
+function deriveActivePlayer(gameTurns) {
+  let currentPlayer = "X"
 
-    function handleSelectSquare(rowIndex, colIndex) {
-      setIsActivePlayer((currActivePlayer) => currActivePlayer === 'X' ? 'O' : 'X')
-      setGameTurns((prevTurns) => {
-        let currentPlayer = "X"
-
-        if(prevTurns.length > 0 && prevTurns[0].player === 'X') {
+        if(gameTurns.length > 0 && gameTurns[0].player === 'X') {
           currentPlayer = 'O';
         }
+        return currentPlayer
+}
 
+
+function App() {
+  const [gameTurns, setGameTurns] = useState([])
+  // const [activePlayer, setIsActivePlayer] = useState('X')
+
+   const activePlayer = deriveActivePlayer(gameTurns)
+
+    function handleSelectSquare(rowIndex, colIndex) {
+      // setIsActivePlayer((currActivePlayer) => currActivePlayer === 'X' ? 'O' : 'X')
+      setGameTurns((prevTurns) => {
+        const currentPlayer = deriveActivePlayer(prevTurns)
         const updatedTurns = [
           { square: {row: rowIndex, col: colIndex }, player: currentPlayer},...prevTurns, ]
           return updatedTurns
